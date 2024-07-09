726ac48eae6c2119b86165a10b12a2b09212e03d
Valentin Chianese <chianesevalentin@gmail.com>
2024-07-09 18:34:31 -0300

    migrated astro project version

---
+++ src/utilities/projects.js
@@ -0,0 +1,430 @@
+export const allProjects = [
+    {
+        projectClass: "web-dev",
+        projectImage: "devtoolskit.png",
+        projectTitle: "Devtoolskit",
+        projectDescription: "Web Astro hecho para desarrolladores",
+        projectTechs: [
+            {
+                img: "astro.svg",
+                title: "Astro"
+            },
+            {
+                img: "typescript.svg",
+                title: "TypeScript"
+            },
+            {
+                img: "tailwind.svg",
+                title: "Tailwind CSS"
+            }
+        ],
+        projectLinks: [
+            {
+                href: "https://devtoolskit.netlify.app/",
+                title: "Sitio web",
+                icon: "fas fa-external-link-alt"
+            },
+            {
+                href: "https://github.com/ImLevan/devtools-site/",
+                title: "Link al repo",
+                icon: "fab fa-github"
+            },
+        ]
+    },
+    {
+        projectClass: "app-dev",
+        projectImage: "inventoryManagement.jpg",
+        projectTitle: "Inventory Management",
+        projectDescription: "App java gestión de inventario y ventas",
+        projectTechs: [
+            {
+                img: "java.svg",
+                title: "Java"
+            },
+            {
+                img: "psql.svg",
+                title: "PostgreSQL"
+            },
+            {
+                img: "git.svg",
+                title: "GIT"
+            }
+        ],
+        projectLinks: [
+            {
+                href: "https://github.com/ImLevan/inventory-management",
+                title: "Link al repo",
+                icon: "fab fa-github"
+            },
+            {
+                href: "https://github.com/ImLevan/inventory-management/blob/master/README.md",
+                title: "Presentación de la app",
+                icon: "fa-solid fa-circle-info"
+            },
+            {
+                href: "https://docs.google.com/document/d/1VV7-y1ycgLpaTwBV3YA210YF9Iwq_C9MfiGH5S7s7c4/edit?usp=sharing",
+                title: "Documentación técnica",
+                icon: "fas fa-book"
+            }
+        ]
+    },
+    {
+        projectClass: "app-dev",
+        projectImage: "FitOps.png",
+        projectTitle: "Fit Ops - Management System",
+        projectDescription: "App java de gestión para gimnasios",
+        projectTechs: [
+            {
+                img: "java.svg",
+                title: "Java"
+            },
+            {
+                img: "psql.svg",
+                title: "PostgreSQL"
+            },
+            {
+                img: "git.svg",
+                title: "GIT"
+            }
+        ],
+        projectLinks: [
+            {
+                href: "https://github.com/ImLevan/Fit-Ops",
+                title: "Link al repo",
+                icon: "fab fa-github"
+            },
+            {
+                href: "https://github.com/ImLevan/Fit-Ops/blob/master/README.md",
+                title: "Presentación de la app",
+                icon: "fa-solid fa-circle-info"
+            },
+            {
+                href: "https://docs.google.com/document/d/1Ib_MgmMK8CmQHtCWghYcJ8DVYipcR7gc2sPzEKmKQ6I/edit?usp=sharing",
+                title: "Documentación técnica",
+                icon: "fas fa-book"
+            }
+        ]
+    },
+    {
+        projectClass: "web-dev",
+        projectImage: "proyectoAnimeTracker.png",
+        projectTitle: "Anime Tracker",
+        projectDescription: "App web de seguimiento de tus animes",
+        projectTechs: [
+            {
+                img: "html.svg",
+                title: "HTML"
+            },
+            {
+                img: "css.svg",
+                title: "CSS"
+            },
+            {
+                img: "js.svg",
+                title: "JavaScript"
+            },
+            {
+                img: "react.svg",
+                title: "React"
+            },
+            {
+                img: "psql.svg",
+                title: "PostgreSQL"
+            },
+            {
+                img: "java.svg",
+                title: "Java"
+            },
+            {
+                img: "spring.png",
+                title: "Spring Boot"
+            }
+        ],
+        projectLinks: [
+            {
+                href: "https://github.com/ImLevan/Anime-Project-Frontend",
+                title: "Link al repo frontend",
+                icon: "fab fa-github"
+            },
+            {
+                href: "https://docs.google.com/document/d/1xDw5uyNbqE8nXngSEBB_Nk6daVJ8FMuE7e1n8GSTn70/edit?usp=sharing",
+                title: "Presentación de la app",
+                icon: "fa-solid fa-circle-info"
+            },
+            {
+                href: "https://anime-project-frontend-m9d6.vercel.app/",
+                title: "Sitio web",
+                icon: "fas fa-external-link-alt"
+            }
+        ]
+    },
+    {
+        projectClass: "web-dev",
+        projectImage: "showflicks.png",
+        projectTitle: "Show Flicks",
+        projectDescription: "Comunicación con API de películas",
+        projectTechs: [
+            {
+                img: "ts.svg",
+                title: "TypeScript"
+            },
+            {
+                img: "react.svg",
+                title: "React"
+            },
+            {
+                img: "tailwind.svg",
+                title: "Tailwind CSS"
+            }
+        ],
+        projectLinks: [
+            {
+                href: "https://github.com/ImLevan/show-flicks",
+                title: "Link al repo",
+                icon: "fab fa-github"
+            },
+            {
+                href: "https://show-flicks.netlify.app/",
+                title: "Sitio web",
+                icon: "fas fa-external-link-alt"
+            }
+        ]
+    },
+    {
+        projectClass: "web-dev",
+        projectImage: "proyectoKarU.png",
+        projectTitle: "KarU - Compra y Venta",
+        projectDescription: "Página web de compra y venta de vehículos",
+        projectTechs: [
+            {
+                img: "html.svg",
+                title: "HTML"
+            },
+            {
+                img: "css.svg",
+                title: "CSS"
+            },
+            {
+                img: "js.svg",
+                title: "JavaScript"
+            },
+            {
+                img: "react.svg",
+                title: "React"
+            },
+            {
+                img: "psql.svg",
+                title: "PostgreSQL"
+            },
+            {
+                img: "java.svg",
+                title: "Java"
+            },
+            {
+                img: "spring.png",
+                title: "Spring Boot"
+            }
+        ],
+        projectLinks: [
+            {
+                href: "https://www.canva.com/design/DAFmYuhsdcc/kwEgxxoLZ7mjLVykYucEaQ/view?utm_content=DAFmYuhsdcc&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink#1",
+                title: "Presentación de la app",
+                icon: "fa-solid fa-circle-info"
+            },
+            {
+                href: "https://github.com/martinzav99/KarU-adminBackend",
+                title: "Link al repo",
+                icon: "fab fa-github"
+            },
+            {
+                href: "https://karu-web-back.onrender.com/",
+                title: "Sitio web",
+                icon: "fas fa-external-link-alt"
+            },
+            {
+                href: "https://docs.google.com/document/d/1-6BEFcK2kuAZaM2AzUBnTX_jSIp62mKq7ibSZFRlkPs/edit?usp=sharing",
+                title: "Documentación",
+                icon: "fas fa-book"
+            }
+        ]
+    },
+    {
+        projectClass: "web-dev",
+        projectImage: "proyecyoMaraton.png",
+        projectTitle: "Página Web de Maratón",
+        projectDescription: "Diseño web inscripción a maratón",
+        projectTechs: [
+            {
+                img: "html.svg",
+                title: "HTML"
+            },
+            {
+                img: "css.svg",
+                title: "CSS"
+            },
+            {
+                img: "js.svg",
+                title: "JavaScript"
+            },
+            {
+                img: "bootstrap.svg",
+                title: "Bootstrap"
+            }
+        ],
+        projectLinks: [
+            {
+                href: "https://github.com/ImLevan/Maqueta-Maraton",
+                title: "Link al repo",
+                icon: "fab fa-github"
+            },
+            {
+                href: "https://maratonmaqueta.netlify.app/",
+                title: "Sitio web",
+                icon: "fas fa-external-link-alt"
+            },
+            {
+                href: "https://docs.google.com/document/d/1-PALzokz7R_kUxCpsFUj7PZu1WffrhT6ZYkp7JRXV-4/edit?usp=sharing",
+                title: "Documentación",
+                icon: "fas fa-book"
+            }
+        ]
+    },
+    {
+        projectClass: "app-dev",
+        projectImage: "JuegoPy.png",
+        projectTitle: "Adivina la canción",
+        projectDescription: "Juego en Python",
+        projectTechs: [
+            {
+                img: "python.svg",
+                title: "Python"
+            },
+            {
+                img: "git.svg",
+                title: "GIT"
+            }
+        ],
+        projectLinks: [
+            {
+                href: "https://github.com/ImLevan/Juego-PY",
+                title: "Link al repo",
+                icon: "fab fa-github"
+            }
+        ]
+    },
+    {
+        projectClass: "app-dev",
+        projectImage: "JuegoNinjas.png",
+        projectTitle: "Naruto Delivery Challenge",
+        projectDescription: "Juego en JAVA",
+        projectTechs: [
+            {
+                img: "java.svg",
+                title: "Java"
+            },
+            {
+                img: "git.svg",
+                title: "GIT"
+            }
+        ],
+        projectLinks: [
+            {
+                href: "https://github.com/ImLevan/Juego-Ninjas",
+                title: "Link al repo",
+                icon: "fab fa-github"
+            }
+        ]
+    },
+    {
+        projectClass: "app-dev",
+        projectImage: "proyectoAhorcado.png",
+        projectTitle: "Ahorcado",
+        projectDescription: "Juego en JAVA",
+        projectTechs: [
+            {
+                img: "java.svg",
+                title: "Java"
+            },
+            {
+                img: "git.svg",
+                title: "GIT"
+            }
+        ],
+        projectLinks: [
+            {
+                href: "https://github.com/ImLevan/Ahorcado",
+                title: "Link al repo",
+                icon: "fab fa-github"
+            }
+        ]
+    },
+    {
+        projectClass: "web-dev",
+        projectImage: "proyecto ecommerce.png",
+        projectTitle: "Ecommerce Estático",
+        projectDescription: "Diseño Web Ecommerce",
+        projectTechs: [
+            {
+                img: "html.svg",
+                title: "HTML"
+            },
+            {
+                img: "css.svg",
+                title: "CSS"
+            },
+            {
+                img: "js.svg",
+                title: "JavaScript"
+            }
+        ],
+        projectLinks: [
+            {
+                href: "https://github.com/ImLevan/project-ecommerce",
+                title: "Link al repo",
+                icon: "fab fa-github"
+            },
+            {
+                href: "https://shoesecommercestatic.netlify.app/",
+                title: "Sitio web",
+                icon: "fas fa-external-link-alt"
+            },
+            {
+                href: "https://docs.google.com/document/d/1XlpUvMKgA66SPRQVDle1bBvwL0878NE5-qdwX3Zbsrg/edit?usp=sharing",
+                title: "Documentación",
+                icon: "fas fa-book"
+            }
+        ]
+    },
+    {
+        projectClass: "web-dev",
+        projectImage: "proyectoClima.png",
+        projectTitle: "Aplicación del Clima - OpenWeather API",
+        projectDescription: "Diseño Web API",
+        projectTechs: [
+            {
+                img: "html.svg",
+                title: "HTML"
+            },
+            {
+                img: "css.svg",
+                title: "CSS"
+            },
+            {
+                img: "js.svg",
+                title: "JavaScript"
+            }
+        ],
+        projectLinks: [
+            {
+                href: "https://github.com/ImLevan/Proyecto-Clima",
+                title: "Link al repo",
+                icon: "fab fa-github"
+            },
+            {
+                href: "https://weather-levanproject.netlify.app/",
+                title: "Sitio web",
+                icon: "fas fa-external-link-alt"
+            }
+        ]
+    }
+]
---
+++ src/utilities/skills.js
@@ -0,0 +1,128 @@
+export const frontendSkills = [
+    {
+        img: "/img/html.svg",
+        title: "HTML",
+        description: "Marcado web"
+    },
+    {
+        img: "/img/css.svg",
+        title: "CSS",
+        description: "Estilos web"
+    },
+    {
+        img: "/img/js.svg",
+        title: "JavaScript",
+        description: "Interactividad web"
+    },
+    {
+        img: "/img/react.svg",
+        title: "React",
+        description: "Librería de JS"
+    },
+    {
+        img: "/img/nextjs.svg",
+        title: "Next.js",
+        description: "Framework de React"
+    },
+    {
+        img: "/img/astro.svg",
+        title: "Astro",
+        description: "Framework web"
+    },
+    {
+        img: "/img/bootstrap.svg",
+        title: "Bootstrap",
+        description: "Framework para diseño web"
+    },
+    {
+        img: "/img/figma.svg",
+        title: "Figma",
+        description: "Diseño colaborativo"
+    },
+    {
+        img: "/img/tailwind.svg",
+        title: "Tailwind",
+        description: "Framework CSS"
+    },
+    {
+        img: "/img/ts.svg",
+        title: "TypeScript",
+        description: "Superconjunto de JS"
+    }
+];
+
+export const backendSkills = [
+    {
+        img: "/img/java.svg",
+        title: "Java",
+        description: "Lenguaje backend"
+    },
+    {
+        img: "/img/nodejs.svg",
+        title: "Node.js",
+        description: "Entorno de JS"
+    },
+    {
+        img: "/img/python.svg",
+        title: "Python",
+        description: "Programación alto nivel"
+    },
+    {
+        img: "/img/django.svg",
+        title: "Django",
+        description: "Framework de Python"
+    },
+    {
+        img: "/img/spring.png",
+        title: "Spring Boot",
+        description: "Herramienta de Spring"
+    }
+];
+
+export const otherSkills = [
+    {
+        img: "/img/mongo.svg",
+        title: "MongoDB",
+        description: "Base de datos NoSQL"
+    },
+    {
+        img: "/img/postman.svg",
+        title: "Postman",
+        description: "Herramienta para APIs"
+    },
+    {
+        img: "/img/mysql.svg",
+        title: "MySQL",
+        description: "Base de datos SQL"
+    },
+    {
+        img: "/img/vscode.svg",
+        title: "VS Code",
+        description: "Editor de código"
+    },
+    {
+        img: "/img/psql.svg",
+        title: "PostgreSQL",
+        description: "Base de datos SQL"
+    },
+    {
+        img: "/img/selenium.svg",
+        title: "Selenium",
+        description: "Entorno de pruebas web"
+    },
+    {
+        img: "/img/sqlite.svg",
+        title: "SQLite",
+        description: "Base de datos SQL"
+    },
+    {
+        img: "/img/git.svg",
+        title: "GIT",
+        description: "Control de versiones"
+    },
+    {
+        img: "/img/linux.svg",
+        title: "Linux",
+        description: "Sistema operativo"
+    }
+];
